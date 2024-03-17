5e3de6e57d92bf34126f41f34d11b8a144b274f4
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-17 19:03:25 -0300

    Create questao2.py

---
+++ questao2.py
@@ -0,0 +1,10 @@
+print('MMC de dois números\n--------------------')
+
+#Solicita os dados ao usuário
+valor_a = int(input('Digite o 1º valor: '))
+valor_b = int(input('Digite o 2º valor: '))
+
+mmc = 1
+while mmc % valor_a != 0 or mmc % valor_b != 0: #Verifica se a variavel é o mmc de ambos os valores
+    mmc += 1
+print(f'__________________\nO MMC de {valor_a} e {valor_b} é [ {mmc} ]')

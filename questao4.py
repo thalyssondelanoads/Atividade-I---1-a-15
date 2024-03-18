552183beab41b6b983ba80b5e2a41d5d761b21c3
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-18 20:01:36 -0300

    Update questao4.py

--- questao4.py
+++ questao4.py
@@ -1,10 +1,13 @@
-print('Divisões sucessivas por 2')
-print('>> Até a divisão ser menor que 1\n------------------')
-n = int(input('Número: ')) #Solicita os dados ao usuário
+def main():
+  print('Divisões sucessivas por 2')
+  print('>> Até a divisão ser menor que 1\n------------------')
+  n = int(input('Número: '))
+  
+  sequencia = 0
+  resultado = 1
+  while resultado >= 1:
+      resultado = n / 2
+      n = resultado
+  print(f'_____________________\nÚltima divisão sucessiva por 2 realizada: {n}')
 
-sequencia = 0
-resultado = 1
-while resultado >= 1: #Divide o valor n por 2 sucessivamente
-    resultado = n / 2
-    n = resultado
-print(f'_____________________\nÚltima divisão sucessiva por 2 realizada: {n}')
+main()

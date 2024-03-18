6e605802aa268a04e45947e4245d79bd4c0a783f
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-18 20:03:24 -0300

    Update questao5.py

--- questao5.py
+++ questao5.py
@@ -1,11 +1,14 @@
-print('Divisão caótica')
-print('>> Até N ser igual a 2\n-------------------------')
-x = int(input('Digite o 1º valor: ')) #Solicita os dados ao usuário
-n = int(input('Digite o 2º valor: '))
+def main():
+  print('Divisão caótica')
+  print('>> Até N ser igual a 2\n-------------------------')
+  x = int(input('Digite o 1º valor: ')) 
+  n = int(input('Digite o 2º valor: '))
+  
+  resultado = 1
+  while n >= 2:
+      resultado = x / n
+      print(f' {x} / {n} = {resultado}')
+      n -= 1
+      x = resultado
 
-resultado = 1
-while n >= 2: #Enquanto n não for 2, a variavel x recebe o valor de resultado e é subtraído 1 da variável n
-    resultado = x / n
-    print(f' {x} / {n} = {resultado}')
-    n -= 1
-    x = resultado
+main()

e2ccda1ba0b5daa50e5f0f0a35210d9a1b70c3c4
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-18 20:32:12 -0300

    Update questao2.py

--- questao2.py
+++ questao2.py
@@ -1,11 +1,11 @@
 def main():
   print('MMC de Dois Números\n--------------------')
-  valor_a = int(input('Digite o 1º valor: '))
-  valor_b = int(input('Digite o 2º valor: '))
+  Valor_a = int(input('Digite o 1º valor: '))
+  Valor_b = int(input('Digite o 2º valor: '))
   
-  mmc = 1
-  while mmc % valor_a != 0 or mmc % valor_b != 0:
-      mmc += 1
-  print(f'__________________\nO MMC de {valor_a} e {valor_b} é [ {mmc} ]')
+  Mmc = 1
+  while Mmc % Valor_a != 0 or Mmc % Valor_b != 0:
+      Mmc += 1
+  print(f'__________________\nO MMC de {Valor_a} e {Valor_b} é [ {Mmc} ]')
 
 main()

5782db5d3d218d4dc89e6825dabeb842d85634b1
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-18 19:57:24 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -1,12 +1,15 @@
-print('Divisores dos números')
-print('>> Digite 0 para sair\n------------------')
+def main():
+  print('Divisores dos números')
+  print('>> Digite 0 para sair\n------------------')
+  
+  n = '' 
+  while n != 0: 
+      n = int(input('Digite o número: '))
+      sequencia = 1
+      while sequencia <= n: 
+          if n % sequencia == 0:
+              print(f'{sequencia}')
+          sequencia += 1
+  print('======= FINALIZADO =======')
 
-n = '' 
-while n != 0: #Verifica se a variável NÃO é o flag número(0)
-    n = int(input('Digite o número: ')) #Solicita os dados ao usuário
-    sequencia = 1
-    while sequencia <= n: 
-        if n % sequencia == 0: #Verifica se cada valor é divisível pelo número
-            print(f'{sequencia}')
-        sequencia += 1
-print('======= FINALIZADO =======')
+main()

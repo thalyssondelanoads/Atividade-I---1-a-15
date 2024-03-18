dc7db0a46a78227bb7f612235f30f847ab2912e7
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-18 20:31:24 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -2,12 +2,12 @@ def main():
   print('Divisores dos números')
   print('>> Digite 0 para sair\n------------------')
   
-  n = '' 
-  while n != 0: 
-      n = int(input('Digite o número: '))
+  N = '' 
+  while N != 0: 
+      N = int(input('Digite o número: '))
       Sequencia = 1
-      while Sequencia <= n: 
-          if n % Sequencia == 0:
+      while Sequencia <= N: 
+          if N % Sequencia == 0:
               print(f'{Sequencia}')
           Sequencia += 1
   print('======= FINALIZADO =======')

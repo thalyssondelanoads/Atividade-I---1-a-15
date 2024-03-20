db5b829ce65c0a6b5f3ff4431cb9d891b1e98b02
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-19 21:40:58 -0300

    Update questao6.py

--- questao6.py
+++ questao6.py
@@ -1,1 +1,11 @@
-.
+def main():
+  print('Número de Dígitos de um Número\n-----------------------------')
+  numero = abs(int(input('Digite o número: ')))
+
+  digitos = 0
+  while numero % 10 > 1:
+    numero /= 10
+    digitos += 1
+  print(f'____________\nO Número tem {digitos} Dígito(s)')
+     
+main()

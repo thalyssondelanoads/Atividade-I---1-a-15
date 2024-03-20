bf759a78126e120a8fdc1f83d0afa0d9eb94b181
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-19 22:32:44 -0300

    Update questao3.py

--- questao3.py
+++ questao3.py
@@ -1,1 +1,12 @@
-.
+def main():
+  print('MDC de Dois Números\n---------------')
+  numero1 = int(input('Digite o 1º Valor: '))
+  numero2 = int(input('Digite o 2º Valor: '))
+  
+  a, b = numero1, numero2
+  while b != 0:
+      a, b = b, a % b
+  resultado_mdc = a
+  print(f'_________________\nO MDC de {numero1} e {numero2} é [{resultado_mdc}]')
+
+main()

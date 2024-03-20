f02cb08c4eb6a56a222e1531588a9a0f6474c55c
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-19 23:05:08 -0300

    Update questao8.py

--- questao8.py
+++ questao8.py
@@ -1,1 +1,14 @@
-.
+def main():
+  print('Soma de Dois Números Consecutivos Igual a X\n------------------')
+  x = int(input('Digite o Número X: '))
+
+  numero = 0
+  numero2 = 0
+  while numero + numero2 != x:
+      numero = int(input('Digite um Número: '))
+      numero2 = int(input('Digite outro Número: '))
+      
+      if numero + numero2 == x:
+        print(f'__________________\nA Soma de {numero} + {numero2} é igual a {x}')
+
+main()

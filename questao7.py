3f6ede554edc314417a0fd0e21436a5080e9f452
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-19 21:42:28 -0300

    Update questao7.py

--- questao7.py
+++ questao7.py
@@ -1,11 +1,11 @@
 def main():
-  print('Número igual ao 1º número\n------------------------------')
-  n1 = float(input('Digite um número: '))
+  print('Número Igual ao 1º Número\n------------------------------')
+  n1 = float(input('Digite um Número: '))
   print('__________________________')
-  
+
   n = 0
   while n != n1:
-      n = float(input('Digite outro número: '))
-  print(f'-------------------------------\n0 número {n} foi o primeiro a ser digitado')
+      n = float(input('Digite outro Número: '))
+  print(f'-------------------------------\n0 Número {n} foi o Primeiro a ser Digitado')
 
 main()
